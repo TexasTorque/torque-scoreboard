4a725c979ec622e8a413605a446872da8a9ee44e
humandavey <davey.adams.three@gmail.com>
2024-02-21 22:24:37 -0600

    Start scoring page design

--- client/src/pages/Scoring.tsx
+++ client/src/pages/Scoring.tsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
+import useWindowSize from "../data/WindowSize";
+import { Image } from "react-bootstrap";
+import scoring_img from "../img/scoring.png";
+
 export default () => {
+  const [scale, setScale] = useState(window.innerWidth / 1920);
+  const size = useWindowSize();
+
+  useEffect(()=> {
+    setScale(size.width / 1920);
+  }, [size.width]);
+
   return (
     <>
-      <h1>Scoring</h1>
+      <Image src={scoring_img} style={{width: size.width}}/>
     </>
   )
 }
