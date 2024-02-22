c40be4fdf1404add27060d80d15750777d078dd7
humandavey <davey.adams.three@gmail.com>
2024-02-21 18:00:00 -0600

    Add types

--- client/src/data/Types.ts
+++ client/src/data/Types.ts
@@ -1,1 +1,17 @@
-export {}
+export interface Match {
+  red: Alliance;
+  blue: Alliance;
+  matchStart: number;
+}
+
+export interface Alliance {
+  teams: Team[];
+  score: number;
+  coopertition: boolean;
+  superchargeStart: number;
+}
+
+export interface Team {
+  name: string;
+  number: number;
+}
